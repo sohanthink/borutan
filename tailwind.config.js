25b9440f941d6534b505d21bee539c3a5a029b80
Muhammad Sohan Mollah <sohansourab@gmail.com>
2024-04-17 14:53:20 +0600

    pages setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./index.html", "./signin.html"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./index.html",
+    "./signin.html",
+    "./contact.html",
+    "./pricing.html",
+  ],
   theme: {
     fontFamily: {
       primary: ["Karla"],
